b1b9af5f7690af60e636755ee3c673c09a504e1b
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-06-29 22:42:26 +0900

    新規作成

---
+++ IssueBox/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using IssueBox.Views;
+
+namespace IssueBox
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Login());
+            Application.Run(new MainWindow());
+        }
+    }
+}
