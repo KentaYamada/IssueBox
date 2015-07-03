e1f973d6e771100e16cf5c82ceabbe29bb7023ea
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-07-04 03:32:16 +0900

    エラーログ機能実装

--- IssueBox.Views/Infrastructure/Logger.cs
+++ IssueBox.Views/Infrastructure/Logger.cs
@@ -10,27 +10,41 @@ namespace IssueBox.Views.Infrastructure
 {
     public static class Logger
     {
-        private static readonly string _confString = ConfigurationManager.AppSettings["Logger"];
+        private static readonly string _confString = ConfigurationManager.AppSettings["LogPath"];
 
-        private static void Write(Exception ex)
+        private static readonly Encoding _enc = Encoding.GetEncoding("utf-8");
+
+        private static void Write(string message)
         {
             if (!Directory.Exists(_confString)) 
             {
                 Directory.CreateDirectory(_confString);
             }
 
-            var sb = new StringBuilder();
-            sb.Length = 0;
-            sb.AppendLine("エラーメッセージ");
-            sb.AppendLine(ex.Message);
-
+            string fileName = string.Format("{0}\\{1}.log", _confString, DateTime.Now.ToString("yyyyMMdd"));
+            try
+            {
+                using (var stream = new StreamWriter(fileName, true, _enc))
+                {
+                    stream.Write(message);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
         
 
         public static void Error(Exception ex)
         {
-            //ログファイル書き出し
-            Logger.Write(ex);
+            var sb = new StringBuilder();
+            sb.Length = 0;
+            sb.AppendLine(string.Format("エラーメッセージ:{0}", ex.Message));
+            sb.AppendLine(string.Format("発生箇所:{0}", ex.TargetSite));
+            sb.AppendLine(string.Format("スタックトレース:{0}", ex.StackTrace));
+            
+            Logger.Write(sb.ToString());
         }
     }
 }
