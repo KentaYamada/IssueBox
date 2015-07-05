2a43de856e2cfb1171932c722e2455d50aafc319
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-07-05 19:53:58 +0900

    検索画面表示ロジック実装

--- IssueBox.Views/MainWindow.cs
+++ IssueBox.Views/MainWindow.cs
@@ -2,8 +2,13 @@
 using System.Drawing;
 using System.Windows.Forms;
 
+using IssueBox.Views.Infrastructure;
+
 namespace IssueBox.Views
 {
+    /// <summary>
+    /// メインウィンドウ
+    /// </summary>
     public partial class MainWindow : Form
     {
         public MainWindow()
@@ -11,31 +16,54 @@ namespace IssueBox.Views
             InitializeComponent();
         }
 
+        /// <summary>
+        /// メニューボタンクリックイベント
+        /// </summary>
         private void menu_Click(object sender, EventArgs e)
         {
-            this.ShowPanel();
+            try
+            {
+                this.ShowPanel();
+            }
+            catch(Exception ex)
+            {
+                Logger.Error(ex);
+            }
         }
 
+        /// <summary>
+        /// 「ログアウト」クリックイベント
+        /// </summary>
         private void logout_Click(object sender, EventArgs e)
         {
             this.Close();
             this.Dispose();
         }
 
+        /// <summary>
+        /// 検索画面表示
+        /// </summary>
         private void ShowPanel()
         {
-            //var panel = Activator.CreateInstance<SearchProject>();
-
-            //this.Controls.Add(panel);
-            //panel.Location = new Point(0, 0);
-            //panel.Dock = DockStyle.Fill;
-            //panel.BringToFront();
+            try
+            {
+                var panel = Activator.CreateInstance<SearchIssue>();
+                this.Controls.Add(panel);
+                panel.Location = new Point(0, 0);
+                panel.Dock = DockStyle.Fill;
+                panel.BringToFront();
+            }
+            catch
+            {
+                throw;
+            }
+            
             //var form = new EntryMember();
             //var form = new EntryProject();
             //var form = new EntryProduct();
             //var form = new EntryCategory();
-            var form = new EntryIssue();
-            form.ShowDialog();
+            //var form = new EntryIssue();
+            //form.ShowDialog();
         }
     }
 }
