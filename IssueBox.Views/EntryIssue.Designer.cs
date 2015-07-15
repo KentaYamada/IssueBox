566793a9a5c75d69df2ee940bc920eaa18eb6119
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-07-15 13:50:44 +0900

    Form表示位置を中央に設定変更

--- IssueBox.Views/EntryIssue.Designer.cs
+++ IssueBox.Views/EntryIssue.Designer.cs
@@ -317,6 +317,7 @@
             this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "EntryIssue";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "タスク登録";
             this.Load += new System.EventHandler(this.EntryIssue_Load);
             this.ResumeLayout(false);
