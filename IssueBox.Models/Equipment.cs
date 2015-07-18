f189d3ebdd95a6b2f12201e123c0fa1f1b25d635
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-07-18 20:12:50 +0900

    マスタ追加

---
+++ IssueBox.Models/Equipment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IssueBox.Models.Infrastructure;
+
+namespace IssueBox.Models
+{
+    public class Equipment
+    {
+        private SQLCommander _db = null;
+
+        /// <summary>機器ID</summary>
+        public int ID { get; set; }
+
+        /// <summary>型番</summary>
+        public string Name { get; set; }
+
+        /// <summary>メーカーID</summary>
+        public int MakerID { get; set; }
+
+        /// <summary>データ有効可否</summary>
+        public bool EnableFlag { get; set; }
+
+        #region Default constructor
+
+        public Equipment()
+        {
+            this.ID = 0;
+            this.Name = "";
+            this.MakerID = 0;
+            this.EnableFlag = true;
+        }
+
+        #endregion
+    }
+}
---
+++ IssueBox.Models/Maker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IssueBox.Models.Infrastructure;
+
+namespace IssueBox.Models
+{
+    /// <summary>
+    /// メーカーモデル
+    /// </summary>
+    public class Maker
+    {
+        private SQLCommander _db = null;
+
+        /// <summary>メーカーID</summary>
+        public int ID { get; set; }
+
+        /// <summary>メーカー名</summary>
+        public string Name { get; set; }
+
+        /// <summary>データ有効可否</summary>
+        public bool EnableFlag { get; set; }
+
+        #region Default constructor
+
+        public Maker()
+        {
+            this.ID = 0;
+            this.Name = "";
+            this.EnableFlag = true;
+            this._db = new SQLCommander();
+        }
+
+        #endregion
+
+        
+        /// <summary>
+        /// メーカー情報保存
+        /// </summary>
+        /// <param name="equipments">機器情報一覧</param>
+        /// <returns>True:成功 / False:異常</returns>
+        public bool Save(List<Equipment> equipments)
+        {
+            try
+            {
+                return this._db.ExecuteStoredProcedure<Maker, Equipment>("SaveMaker", this, equipments) > 0 ? true : false;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
