b0d83c792b0ededa787cb0ee719d952d8c57dbb6
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-07-29 22:51:56 +0900

    単位マスタ新規作成

---
+++ IssueBox.Models.UnitTest/TestUnit.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+
+using NUnit.Framework;
+using IssueBox.Models.Infrastructure;
+
+namespace IssueBox.Models.UnitTest
+{
+    public class TestUnit
+    {
+        private readonly SQLCommander _db = null;
+
+        #region Default constructor
+
+        public TestUnit()
+        {
+            this._db = new SQLCommander();
+        }
+
+        #endregion
+
+        #region Setup
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            string sql = @"INSERT INTO UNITS VALUES ('kW', 'TRUE', GETDATE())
+                                                   ,('Hz','FALSE', GETDATE())
+                                                   ,('V','TRUE', GETDATE())";
+            try
+            {
+                this._db.Execute(sql);
+                Debug.Print("Setup success.");
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Teardown
+
+        [TestFixtureTearDown]
+        public void Teardown()
+        {
+            string sql = "DELETE FROM UNITS";
+
+            try
+            {
+                this._db.Execute(sql);
+                Debug.Print("Teardown success.");
+            }
+            catch(Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Test FindUnitsBy function
+
+        private static readonly object[] FindUnitsByTestCases = 
+        {
+            new object[] {3, new Condition() { Name = "", EnableFlag = "all" } },
+            new object[] {2, new Condition() { Name = "", EnableFlag = "true" } },
+            new object[] {1, new Condition() { Name = "", EnableFlag = "false" } },
+            new object[] {1, new Condition() { Name = "kW", EnableFlag = "all" } },
+            new object[] {1, new Condition() { Name = "kW", EnableFlag = "true" }},
+            new object[] {0, new Condition() { Name = "kW", EnableFlag = "false" } },
+        };
+
+        [TestCaseSource("FindUnitsByTestCases")]
+        public void TestFindUnitsBy(int expected, Condition condition)
+        {
+            var data = Unit.FindUnitsBy(condition);
+            Assert.AreEqual(expected, data.Count);
+        }
+
+        #endregion
+
+        #region Test Save function
+
+        private static readonly object[] SaveTestCases = 
+        {
+            new object[] { true, new Unit() { ID = 0, Name = "test", EnableFlag = true } },
+            new object[] { false, new Unit() { ID = 0, Name = null, EnableFlag = false } },
+            new object[] { true, new Unit() { ID = 1, Name = "upd_test", EnableFlag = true } },
+            new object[] { false, new Unit() { ID = 0, Name = null, EnableFlag = true } }
+        };
+
+        [TestCaseSource("SaveTestCases")]
+        public void TestSave(bool expected, Unit target)
+        {
+            bool result = false;
+
+            try
+            {
+                result = target.Save();
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+    }
+}
---
+++ IssueBox.Views/EntryUnit.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace IssueBox.Views
+{
+    partial class EntryUnit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new IssueBox.Views.Infrastructure.TextBoxEx();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.grpEnable = new IssueBox.Views.Infrastructure.EnableRadioButtons();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.AlertMessage = "必須入力です。";
+            this.txtName.Location = new System.Drawing.Point(92, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Required = true;
+            this.txtName.Size = new System.Drawing.Size(149, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "単位";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 28);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "データ有効";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // grpEnable
+            // 
+            this.grpEnable.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grpEnable.Enable = true;
+            this.grpEnable.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.grpEnable.Location = new System.Drawing.Point(92, 94);
+            this.grpEnable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpEnable.Name = "grpEnable";
+            this.grpEnable.Size = new System.Drawing.Size(117, 28);
+            this.grpEnable.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(169, 172);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 28);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // EntryUnit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 212);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.grpEnable);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(5, 8, 5, 8);
+            this.Name = "EntryUnit";
+            this.Text = "単位設定";
+            this.Controls.SetChildIndex(this.btnReturn, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.grpEnable, 0);
+            this.Controls.SetChildIndex(this.btnSave, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Infrastructure.TextBoxEx txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private Infrastructure.EnableRadioButtons grpEnable;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
