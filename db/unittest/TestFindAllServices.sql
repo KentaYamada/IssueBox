31e5eaa6cdb32feb219c631b1b8a2e23a9bec742
KentaYamada <kntx.fullspeed.428@gmail.com>
2015-08-10 17:28:34 +0900

    サービス取得処理新規作成

---
+++ db/unittest/TestFindAllServices.sql
@@ -0,0 +1,17 @@
+﻿/*
+ * サービス取得処理テスト
+ */
+use IssueBox_Test
+
+INSERT INTO PRODUCTS VALUES ('Product A', NULL, 1, 'TRUE', GETDATE())
+                           ,('Product B', NULL, 1, 'TRUE', GETDATE())
+                           ,('Service A', NULL, 2, 'TRUE', GETDATE())
+                           ,('Service B', NULL, 2, 'FALSE', GETDATE())
+SELECT * FROM PRODUCTS
+
+EXEC FindAllServices
+
+DELETE FROM PRODUCTS
+
+--連番リセット
+DBCC CHECKIDENT(PRODUCTS, RESEED, 0)
