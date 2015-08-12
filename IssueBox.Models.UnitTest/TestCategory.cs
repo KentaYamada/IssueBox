6b26fe57dc48f59daecdac29f23a07b4adc9f19e
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-08-12 13:36:51 +0900

    メソッド名修正

--- IssueBox.Models.UnitTest/TestCategory.cs
+++ IssueBox.Models.UnitTest/TestCategory.cs
@@ -98,7 +98,7 @@ namespace IssueBox.Models.UnitTest
         [TestCaseSource("TestCases")]
         public void TestFindCategoriesBy(int expected, Condition condition)
         {
-            var data = Category.FindByCategories(condition);
+            var data = Category.FindCategoriesBy(condition);
             Assert.AreEqual(expected, data.Count);
         }
     }
