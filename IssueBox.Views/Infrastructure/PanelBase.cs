5f242c52ffa757a06140c7c14306d49f37c8de09
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-09-06 18:49:59 +0900

    メソッドの共通化(途中)

--- IssueBox.Views/Infrastructure/PanelBase.cs
+++ IssueBox.Views/Infrastructure/PanelBase.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+
 
 namespace IssueBox.Views.Infrastructure
 {
@@ -27,5 +31,58 @@ namespace IssueBox.Views.Infrastructure
         {
             InitializeComponent();
         }
+
+        protected virtual void SetReadData() { }
+
+        //protected virtual void ShowEntryWindow() { }
+
+        ///// <summary>
+        ///// 「新規作成」クリックイベント
+        ///// </summary>
+        //protected void btnNew_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        //ToDo:入力画面表示
+        //        this.ShowEntryWindow();
+        //    }
+        //    catch(SqlException ex)
+        //    {
+        //        Logger.Error(ex);
+        //    }
+        //}
+
+        /// <summary>
+        /// 「検索」クリックイベント
+        /// </summary>
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.SetReadData();
+            }
+            catch (SqlException ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        ///// <summary>
+        ///// 行データ選択イベント
+        ///// </summary>
+        //protected void grdList_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    //ToDo:入力画面表示
+        //    this.ShowEntryWindow();
+
+        //    try
+        //    {
+        //        this.SetReadData();
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        Logger.Error(ex);
+        //    }
+        //}
     }
 }
