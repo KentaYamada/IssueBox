353e20e892c1874ef12a0da7dbd88bbafa261d1c
KentaYamada <kntx.fullspeed.428@gmail.com>
2015-12-11 21:21:42 +0900

    【Fix】 集約例外に変更するため、main関数に例外を全て捕捉するよう変更

--- IssueBox/Program.cs
+++ IssueBox/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+
 using IssueBox.Views;
+using IssueBox.Views.Infrastructure;
 
 namespace IssueBox
 {
@@ -12,10 +15,27 @@ namespace IssueBox
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
             Application.Run(new MainWindow());
+            //Application.Run(new Login());
+        }
+
+        /// <summary>
+        /// 捕捉しきれない例外を補足するイベント
+        /// </summary>
+        public static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            //ユーザーにシステムエラーが発生したことを通知
+            MessageBox.Show("システムエラーが発生しました。\nシステム管理者へお問い合わせ下さい。",
+                            "システムエラー",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+
+            //エラーログ出力
+            Logger.Error(e.Exception);
+            Application.Exit();
         }
     }
 }
