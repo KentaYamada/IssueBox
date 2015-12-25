dba2fbb8ed2a22b45eae4639865a7d8a2d71fc53
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2015-12-26 03:11:51 +0900

    【Upd】 　CSV出力機能実装

--- IssueBox.Models/ModelBase.cs
+++ IssueBox.Models/ModelBase.cs
@@ -1,4 +1,9 @@
-﻿using IssueBox.Models.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using IssueBox.Models.Infrastructure;
 
 namespace IssueBox.Models
 {
@@ -12,5 +17,28 @@ namespace IssueBox.Models
         
         /// <summary>主キー</summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// Model→CSV変換
+        /// </summary>
+        protected static string ModelsToCsv<TModel>(List<TModel> models)
+            where TModel : class
+        {
+            var sb = new StringBuilder();
+            sb.Clear();
+
+            var columns = typeof(TModel).GetProperties().ToList();
+
+            columns.ForEach(x => sb.AppendFormat("{0},", x.Name));
+            sb.Remove(sb.Length - 1, 1).AppendLine();
+
+            foreach (var model in models)
+            {
+                columns.ForEach(x => sb.AppendFormat("{0},", x.GetValue(model, null)));
+                sb.Remove(sb.Length - 1, 1).AppendLine();
+            }
+
+            return sb.ToString();
+        }
     }
 }
